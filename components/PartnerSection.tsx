b9c93ea7d44ff6eaa946c12707e914338fe48a05
zxlvera <xdoki26@gmail.com>
2022-07-12 10:23:48 +0800

    partnership

--- components/PartnerSection.tsx
+++ components/PartnerSection.tsx
@@ -2,12 +2,13 @@ import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
 
 import outdoors from '../images/Outdoors.png';
+import { urlFor } from '../lib/client';
 
 const PartnerSection = ({ data }) => {
   return (
     <section className="blue">
       <div className="content">
-        <h1 className="allcaps center">{data.partnerships[0].title}</h1>
+        <h1 className="allcaps center">{data.aboutPartnerships[0].title}</h1>
         <div className="flex flex-mobile-col">
           <div className="mintingImage">
             <img
@@ -17,11 +18,26 @@ const PartnerSection = ({ data }) => {
             />
           </div>
           <div className="padding-h larger">
-            <PortableText value={data.partnerships[0].description} />
+            <PortableText value={data.aboutPartnerships[0].description} />
             <Link href="https://docs.google.com/presentation/d/1q4STB0d29OUhPYjJID7-h1ArNycrlDRPH0DF7r4zfos/present?slide=id.p">
               Read Our Sponsorship Deck
             </Link>
           </div>
+        </div>
+        <div>
+          {data.partnerships.map((partner) => (
+            <>
+              {partner.image && (
+                <img
+                  src={urlFor(partner.image).url()}
+                  alt={partner.name}
+                  className="circle"
+                  width="200px"
+                />
+              )}
+              <h3 className="margin-top">{partner.name}</h3>
+            </>
+          ))}
         </div>
       </div>
     </section>
